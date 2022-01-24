62a551680d323302d3687cc1a7ea333a45b26010
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-24 12:06:52 +0000

    Java Basics

---
+++ AidenSykes/ConfigCheck/src/main/java/com/sparta/basics/JavaBasics.java
@@ -0,0 +1,13 @@
+package com.sparta.basics;
+
+public class JavaBasics {
+    public static void main(String[] args){
+
+        int myIndex = 0;
+        // float, boolean, byte, short, long, double <-- primitive types
+        // String,  <-- not primitive, capitalised therefore class
+
+    }
+
+
+}
