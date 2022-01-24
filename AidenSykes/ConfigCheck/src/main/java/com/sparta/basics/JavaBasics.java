325a92cacd65a82fbe0ace04841e2ee40843eded
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-24 14:17:08 +0000

    Java Basics

--- AidenSykes/ConfigCheck/src/main/java/com/sparta/basics/JavaBasics.java
+++ AidenSykes/ConfigCheck/src/main/java/com/sparta/basics/JavaBasics.java
@@ -1,5 +1,7 @@
 package com.sparta.basics;
 
+import java.util.Locale;
+
 public class JavaBasics {
     public static void main(String[] args){
 
@@ -8,8 +10,17 @@ public class JavaBasics {
         // String,  <-- not primitive, capitalised therefore class
         boolean isOpen = true;
         String name = "Aiden";
-        String anotherName = "Aiden"; //refers to the same string
-        System.out.println("My name is " + name);
+        String anotherName = "Aiden"; //refers to the same string, works because Strings are immutable. Immutable meaning they cannot be changed.
+        anotherName = name.toUpperCase();
+        System.out.println("My name is " + name); // concatenation
+        System.out.println("In upper case " + anotherName);
+        String testName = new String("Aiden"); //creates a new String in the heap
+        if(name == "Aiden")
+            System.out.println("This is Aiden");
+
+
+
+
     }
 
 
