a206c4ff7549d502c5cd76438b02b8f8aa61dbdc
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-25 16:31:45 +0000

    Java Planet Exercise

--- AidenSykes/ConfigCheck/src/test/java/com/spartaglobal/configtest/test/PlanetsTest.java
+++ AidenSykes/ConfigCheck/src/test/java/com/spartaglobal/configtest/test/PlanetsTest.java
@@ -1,4 +1,31 @@
 package com.spartaglobal.configtest.test;
 
+import com.sparta.basics.Planets;
+import org.junit.jupiter.api.Test;
+
 public class PlanetsTest {
+
+    @Test
+
+    public void testGetMass() {
+
+        Planets planetTester;
+        planetTester = Planets.EARTH;
+        System.out.println(planetTester.mass());
+
+
+    }
+
+    @Test
+
+    public void testGetDistance() {
+
+        Planets planetTester;
+        planetTester = Planets.EARTH;
+        System.out.println(planetTester.distance());
+
+
+    }
+
+
 }
