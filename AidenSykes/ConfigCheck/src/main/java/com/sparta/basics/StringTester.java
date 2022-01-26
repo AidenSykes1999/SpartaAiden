191c1d8f3a038c3da6cb40bb14d7d98f5b2f76d9
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-26 10:34:16 +0000

    Java Heap explanation

--- AidenSykes/ConfigCheck/src/main/java/com/sparta/basics/StringTester.java
+++ AidenSykes/ConfigCheck/src/main/java/com/sparta/basics/StringTester.java
@@ -4,8 +4,15 @@ public class StringTester {
     public static void main(String[]args){
 
         String name = "Aiden Sykes";
-        System.out.println("The person's name is: " + name);
+        System.out.println("The person's name is: " + name); //String Builder like a string but modifyable. Less stress
+//        String newName = new String(name); // String which is on the heap, in addition to one on String pool
+                                            // Can be done for security reasons, data can be deleted safely.
 
+        String surname = name.substring(6); // doesn't create a new object, refers to original String offset 6
+        name = null; // Cannot work, because surname references it. Would be a problem if referring to a huge String.
+        surname = new String(name.substring(7)); // in heap, full name can be removed.
+
+        System.out.printf("The name is: %s", surname); // Where %s substitutes the variable.
 
 
     }
