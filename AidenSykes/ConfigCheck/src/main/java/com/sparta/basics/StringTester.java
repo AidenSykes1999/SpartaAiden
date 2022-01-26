2ddca37c756ddf483c1331139a1af0ba7d7efae7
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-26 09:44:36 +0000

    Java Heap explanation

---
+++ AidenSykes/ConfigCheck/src/main/java/com/sparta/basics/StringTester.java
@@ -0,0 +1,14 @@
+package com.sparta.basics;
+
+public class StringTester {
+    public static void main(String[]args){
+
+        String name = "Aiden Sykes";
+        System.out.println("The person's name is: " + name);
+
+
+
+    }
+
+
+}
