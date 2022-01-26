dc3e4b852c5f1d02a6349cc9579e8a7a330832ad
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-26 14:18:09 +0000

    Object Oriented Encapsulation + Constructors

--- AidenSykes/OOPExample/src/main/java/com/sparta/oop/RetailStore.java
+++ AidenSykes/OOPExample/src/main/java/com/sparta/oop/RetailStore.java
@@ -3,12 +3,7 @@ package com.sparta.oop;
 public class RetailStore {
     public static void main(String[] args) {
 
-        Customer sykes = new Customer();
-        sykes.setFirstName("Aiden");
-        sykes.setLastName("Sykes"); // Use getters and setters
-        sykes.setCustomerId(1);
-
-
+        Customer sykes = new Customer("Aiden", "Sykes", 1);
         System.out.println(sykes.convertToString());
 
 
