00bb26546de8112e9557602969cf43f3fc2fcfc6
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-01-31 12:49:47 +0000

    J-unit testing Parameterized Test CSV / Value Source

--- Junittest/src/test/java/com/spartaglobal/junit/ProgramTests.java
+++ Junittest/src/test/java/com/spartaglobal/junit/ProgramTests.java
@@ -11,26 +11,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ProgramTests {
 
 @ParameterizedTest
+//@ValueSource(ints = {5,8,12})
 @CsvSource({"12,Good Morning", "13, Good Afternoon"})
 
     @DisplayName("Given a time of 12 and 13, I expect appropriate greetings")
     public void give12or13_appropriateValueReturned(int time, String expectedGreeting){
     //Arrange
-
     assertEquals(expectedGreeting,Program.greeting((time)));
-
     //Act
-
     //Assert
-
-
 
     }
 
+    @ParameterizedTest
+@ValueSource(ints = {5,8,12})
+    @DisplayName("Given a time between 5 and 12, I expect good morning")
+    public void give5to12_appropriateValueReturned(int time) {
+        //Arrange
+        assertEquals("Good Morning", Program.greeting((time)));
+        //Act
+        //Assert
 
-
-
-
-
+    }
 
 }
