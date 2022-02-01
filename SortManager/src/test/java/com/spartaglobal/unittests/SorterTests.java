06867db5453de513bc4709a05d9d067e614466d0
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-02-01 02:11:17 +0000

    Unit Tests for sorting algorithms

--- SortManager/src/test/java/com/spartaglobal/unittests/SorterTests.java
+++ SortManager/src/test/java/com/spartaglobal/unittests/SorterTests.java
@@ -1,4 +1,28 @@
 package com.spartaglobal.unittests;
 
+import com.spartaglobal.sortmanager.BubbleSorter;
+import com.spartaglobal.sortmanager.MergeSorter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class SorterTests {
+
+    @Test
+    public void positiveMergeTest() {
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+        int[] expected = { 1, 2, 3, 4, 5, 6 };
+        MergeSorter.mergeSort(actual, actual.length);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void positiveBubbleTest() {
+        int[] actual = {5, 1, 6, 2, 3, 4};
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        BubbleSorter.bubbleSort(actual);
+        assertArrayEquals(expected, actual);
+
+
+    }
 }
