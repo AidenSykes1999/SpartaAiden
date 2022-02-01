0d033615c5413dfa9ea52bec673b1479f4bfda25
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-02-01 14:47:16 +0000

    Day 2  - MVC Sorter created with ability to scan sort choice

--- SortManager/src/main/java/com/spartaglobal/sortmanager/viewer/SortView.java
+++ SortManager/src/main/java/com/spartaglobal/sortmanager/viewer/SortView.java
@@ -1,4 +1,22 @@
 package com.spartaglobal.sortmanager.viewer;
+import java.util.Scanner;
+
 
 public class SortView {
+    public String sortSelector() {
+
+        System.out.println("Which Sort would you like to use?");
+        Scanner scanner = new Scanner(System.in);
+        String selectChoice = scanner.next();
+        return selectChoice;
+
+    }
+
+    public void displaySort(String result) {
+
+        System.out.println(result);
+
+    }
+
+
 }
