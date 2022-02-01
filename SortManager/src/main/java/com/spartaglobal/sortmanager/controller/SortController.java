521e987f1f087e2fb3d78e239e32642ba3011633
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-02-01 19:24:59 +0000

    Day 2  - MVC Factory implementation. TODO -  Controller FIX. Main Method initialised. COMPLETE

--- SortManager/src/main/java/com/spartaglobal/sortmanager/controller/SortController.java
+++ SortManager/src/main/java/com/spartaglobal/sortmanager/controller/SortController.java
@@ -1,7 +1,35 @@
 package com.spartaglobal.sortmanager.controller;
 
+import com.spartaglobal.sortmanager.model.BubbleSorter;
+import com.spartaglobal.sortmanager.model.MergeSorter;
+import com.spartaglobal.sortmanager.model.Sorter;
+import com.spartaglobal.sortmanager.model.SorterFactory;
+
 public class SortController {
 
+    public String initiateDriving(String desiredVehicleType) {
+        Sorter s = getSorter(desiredVehicleType);
+        return s.sort();
+    }
 
+    // simple factory method
+    public Sorter getSorter(String sorterType){
+        SorterFactory sf = switch (sorterType.toLowerCase()) {
+
+            case "bubble":
+                System.out.println("Creating a Bubble Sorter");
+                new BubbleSorter();
+
+
+            case "merge":
+                System.out.println("Creating a Merge Sorter");
+                new MergeSorter();
+
+            default:
+                System.out.println("Invalid Selection");
+
+        };
+        return sf.getInstance();
+    }
 
 }
