5bc2671271e11c0e10b263a4f974571efc94aa18
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2022-02-02 13:29:11 +0000

    Day 3 - Logging implemented TODO - Unit Testing + Improve logging

--- SortManager/src/main/java/com/spartaglobal/sortmanager/SorterMain.java
+++ SortManager/src/main/java/com/spartaglobal/sortmanager/SorterMain.java
@@ -3,16 +3,18 @@ package com.spartaglobal.sortmanager;
 import com.spartaglobal.sortmanager.controller.SortController;
 import com.spartaglobal.sortmanager.viewer.SortView;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
 
 public class SorterMain {
-    //private static LogManager logger = LogManager.getLogManager("Sort Logger");
+    private static Logger logger = LogManager.getLogger("Sort Logger");
     public static void main(String[] args) {
 
-
+        logger.error("Looking for User Input");
         SortView view = new SortView();
         String sortChoice = view.sortSelector();
+        logger.error("Attempting to perform a merge.");
         SortController controller = new SortController();
         String result = controller.initiateSort(sortChoice);
 
